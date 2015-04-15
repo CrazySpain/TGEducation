ff75f0171bf1b8a96261791e00806d40e7d84f02
CrazySpain <toraholdman@gmail.com>
2015-04-15 15:37:12 +0300

    Initial commit.

---
+++ TGEducation/main.m
@@ -0,0 +1,61 @@
+//
+//  main.m
+//  TGEducation
+//
+//  Created by Tora on 4/14/15.
+//  Copyright (c) 2015 Tora. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//int main(int argc, const char * argv[])
+//{
+    
+   // @autoreleasepool {
+        
+       // NSInteger i = 20;
+        
+       // while(i < 31) {
+         //   NSLog(@"%li", i);
+          //  i++;
+        //}
+        
+   // }
+   // return 0;
+//}
+
+
+//int main(int argc, const char * argv[])
+//{
+    
+    //@autoreleasepool {
+    
+        //for(NSInteger i = 20; i < 31; i++) {
+            //NSLog(@"%li", i);
+            
+       // }
+    
+        
+   // }
+    //return 0;
+//}
+
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        NSArray *fuckingArray = [[NSArray alloc]
+    initWithObjects:@"30",@"31",@"32",@"33",@"34",@"35",@"36",@"37",@"38",@"39",@"40",@"41", nil];
+        
+        for(NSInteger i = 30; i < 41; i++) {
+            NSLog(@"%li", i);
+        }
+ }
+    return 0;
+}
+
+
+
+
+

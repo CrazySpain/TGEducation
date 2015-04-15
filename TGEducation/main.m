052bbc075e9f026b0a28f7d1d7a67e6dc8d16aef
CrazySpain <toraholdman@gmail.com>
2015-04-15 16:46:30 +0300

    removed trash.

--- TGEducation/main.m
+++ TGEducation/main.m
@@ -8,52 +8,30 @@
 
 #import <Foundation/Foundation.h>
 
-//int main(int argc, const char * argv[])
-//{
-    
-   // @autoreleasepool {
-        
-       // NSInteger i = 20;
-        
-       // while(i < 31) {
-         //   NSLog(@"%li", i);
-          //  i++;
-        //}
-        
-   // }
-   // return 0;
-//}
-
-
-//int main(int argc, const char * argv[])
-//{
-    
-    //@autoreleasepool {
-    
-        //for(NSInteger i = 20; i < 31; i++) {
-            //NSLog(@"%li", i);
-            
-       // }
-    
-        
-   // }
-    //return 0;
-//}
-
 
 int main(int argc, const char * argv[])
 {
-
+    
     @autoreleasepool {
-        NSArray *fuckingArray = [[NSArray alloc]
-    initWithObjects:@"30",@"31",@"32",@"33",@"34",@"35",@"36",@"37",@"38",@"39",@"40",@"41", nil];
-        
-        for(NSInteger i = 30; i < 41; i++) {
+    
+        for(NSInteger i = 20; i < 31; i++) {
             NSLog(@"%li", i);
+            
         }
- }
+        
+        NSInteger i = 20;
+        
+        while(i < 31) {
+            NSLog(@"%li", i);
+            i++;
+        }
+        
+        
+    }
     return 0;
 }
+
+
 
 
 
